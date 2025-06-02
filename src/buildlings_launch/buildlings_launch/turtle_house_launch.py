2228419e639d249d4e1ef5c8fee10c3468710bfd
lemonlemonde <mirudjun@gmail.com>
2025-06-02 19:52:02 +0000

    :bug: use official ign_gazebo and turtlebot3 launchfiles for material/model paths

--- src/buildlings_launch/buildlings_launch/turtle_house_launch.py
+++ src/buildlings_launch/buildlings_launch/turtle_house_launch.py
@@ -2,47 +2,59 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-    gazebo_pkg = get_package_share_directory('ros_gz_sim')
+    gazebo_pkg = get_package_share_directory('gazebo_ros')
     turtle_pkg = get_package_share_directory('turtlebot3_gazebo')
 
     world_path = os.path.join(
-        get_package_share_directory('turtlebot3_gazebo'), 
-        'models', 
-        'turtlebot3_house',
-        'model.sdf'
+        turtle_pkg, 
+        'worlds', 
+        'turtlebot3_house.world'
     )
-    model_path = os.path.join(
-        get_package_share_directory('turtlebot3_gazebo'),
-        'models',
-        'turtlebot3_burger',
-        'model.sdf'
-    )
+
+    # spawn turtle here
+    x_pose = LaunchConfiguration('x_pose', default='0.0')
+    y_pose = LaunchConfiguration('y_pose', default='0.0')
+    z_pose = LaunchConfiguration('z_pose', default='0.1')
+
+    # all the preconfigged launch files
+    gzserver_launch = os.path.join(gazebo_pkg, 'launch', 'gzserver.launch.py')
+    gzclient_launch = os.path.join(gazebo_pkg, 'launch', 'gzclient.launch.py')
+    robot_state_publisher_launch = os.path.join(turtle_pkg, 'launch', 'robot_state_publisher.launch.py')
+    spawn_turtlebot_launch = os.path.join(turtle_pkg, 'launch', 'spawn_turtlebot3.launch.py')
+
 
     # format the expected launch description
     return LaunchDescription([
-        # launch gazebo via `ros_gz_sim` package
+        # launch gazebo server via their own launch file
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(gazebo_pkg, 'launch', 'gz_sim_launch.py')
-            ),
-            launch_arguments={'gz_args': f'-r {world_path}'}.items()
+            PythonLaunchDescriptionSource(gzserver_launch),
+            launch_arguments={'world': world_path}.items(),
         ),
 
-        # add turtle
-            # using same `ros_gz_sim` package, create/spawn a thing!
-        Node(
-            package='ros_gz_sim',
-            executable='create',
-            arguments=[
-                '-name', 'turtlebot3',
-                '-x', '0', '-y', '0', '-z', '0.1',
-                '-file', os.path.join(turtle_pkg, 'models', 'turtlebot3_burger', 'model.sdf')
-            ],
-            output='screen'
+        # gazebo client via their own launch file
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(gzclient_launch),
+        ),
+
+        # turtle publisher node launch via their own launch file
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(robot_state_publisher_launch),
+            launch_arguments={'use_sim_time': 'true'}.items(),
+        ),
+
+        # actual turtle via their own launch file
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(spawn_turtlebot_launch),
+            launch_arguments={
+                'x_pose': x_pose,
+                'y_pose': y_pose,
+                'z_pose': z_pose
+            }.items(),
         ),
 
         # # Start your teleop/drive/control node here if you want
