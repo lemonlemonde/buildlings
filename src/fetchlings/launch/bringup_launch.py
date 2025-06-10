885845923604e5d885dc14baa294bc6696513d40
lemonlemonde <mirudjun@gmail.com>
2025-06-10 13:37:44 -0700

    :construction: WIP Collapsed slam_launch and slam_toolbox into bringup_launch.py

--- src/fetchlings/launch/bringup_launch.py
+++ src/fetchlings/launch/bringup_launch.py
@@ -26,7 +26,7 @@ from launch.actions import (
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PythonExpression
-from launch_ros.actions import PushRosNamespace
+from launch_ros.actions import PushRosNamespace, Node
 
 
 def generate_launch_description():
@@ -41,6 +41,7 @@ def generate_launch_description():
     # Get the launch directory of map_merge
     map_merge_dir = get_package_share_directory("multirobot_map_merge")
     map_merge_launch_dir = os.path.join(map_merge_dir, "launch", "tb3_simulation")
+    
 
     # Create the launch configuration variables
     namespace = LaunchConfiguration("namespace")
@@ -73,11 +74,6 @@ def generate_launch_description():
     declare_slam_toolbox_cmd = DeclareLaunchArgument(
         "slam_toolbox", default_value="True", description="Whether run a SLAM toolbox"
     )
-    declare_slam_gmapping_cmd = DeclareLaunchArgument(
-        "slam_gmapping",
-        default_value="False",
-        description="Whether run a SLAM gmapping",
-    )
 
     declare_map_yaml_cmd = DeclareLaunchArgument(
         "map", description="Full path to map yaml file to load"
@@ -101,39 +97,108 @@ def generate_launch_description():
         description="Automatically startup the nav2 stack",
     )
 
+    # collapsed slam_launch.py
+        # https://github.com/ros-navigation/navigation2/blob/0cdf4f44c7a4171bfa4c6eb885d46c3c19f26a87/nav2_bringup/bringup/launch/slam_launch.py
+    # slam_launch.py
+    slam_toolbox_dir = get_package_share_directory('slam_toolbox')
+    # TODO: collapse this launch file too?
+    slam_launch_file = os.path.join(slam_toolbox_dir, 'launch', 'online_sync_launch.py')
+    lifecycle_nodes = ['map_saver']
+    start_slam_toolbox_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(slam_launch_file),
+        launch_arguments={'use_sim_time': use_sim_time}.items())
+    start_map_saver_server_cmd = Node(
+        package='nav2_map_server',
+        node_executable='map_saver_server',
+        output='screen',
+        parameters=[params_file])
+    start_lifecycle_manager_cmd = Node(
+            package='nav2_lifecycle_manager',
+            node_executable='lifecycle_manager',
+            node_name='lifecycle_manager_slam',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time},
+                        {'autostart': autostart},
+                        {'node_names': lifecycle_nodes}])
+    
+    # Collapsed slam_toolbox.py
+    slam_params_file = LaunchConfiguration("slam_params_file")
+    remappings = [
+        ("/map", "map"),
+        ("/scan", "scan"),
+        ("/tf", "tf"),
+        ("/tf_static", "tf_static"),
+    ]
+    
+    declare_slam_params_file_cmd = DeclareLaunchArgument(
+        "slam_params_file",
+        default_value=os.path.join(
+            get_package_share_directory("slam_toolbox"),
+            "config",
+            "mapper_params_online_sync.yaml",
+        ),
+        description="Full path to the ROS2 parameters file to use for the slam_toolbox node",
+    )
+
+    start_sync_slam_toolbox_node = Node(
+        parameters=[slam_params_file, {"use_sim_time": use_sim_time}],
+        package="slam_toolbox",
+        executable="sync_slam_toolbox_node",
+        name="slam_toolbox",
+        output="screen",
+        remappings=remappings,
+    )
+    
+    
     # Specify the actions
     bringup_cmd_group = GroupAction(
         [
             PushRosNamespace(condition=IfCondition(use_namespace), namespace=namespace),
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(launch_dir, "slam_launch.py")
-                ),
-                condition=IfCondition(
-                    PythonExpression(
-                        [slam, " and ", slam_toolbox, " and not ", slam_gmapping]
-                    )
-                ),
-                launch_arguments={
-                    "namespace": namespace,
-                    "use_sim_time": use_sim_time,
-                    "autostart": autostart,
-                    "params_file": params_file,
-                }.items(),
-            ),
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(map_merge_launch_dir, "slam_toolbox.py")
-                ),
-                condition=IfCondition(
-                    PythonExpression(
-                        [slam, " and ", slam_toolbox, " and not ", slam_gmapping]
-                    )
-                ),
-                launch_arguments={
-                    "use_sim_time": use_sim_time,
-                }.items(),
-            ),
+            # IncludeLaunchDescription(
+            #     PythonLaunchDescriptionSource(
+            #         os.path.join(launch_dir, "slam_launch.py")
+            #     ),
+            #     condition=IfCondition(
+            #         PythonExpression(
+            #             [slam, " and ", slam_toolbox, " and not ", slam_gmapping]
+            #         )
+            #     ),
+            #     launch_arguments={
+            #         "namespace": namespace,
+            #         "use_sim_time": use_sim_time,
+            #         "autostart": autostart,
+            #         "params_file": params_file,
+            #     }.items(),
+            # ),
+
+            # Collapsed slam_launch.py
+            start_slam_toolbox_cmd,
+            start_map_saver_server_cmd,
+            start_lifecycle_manager_cmd,
+
+            
+            
+            # IncludeLaunchDescription(
+            #     PythonLaunchDescriptionSource(
+            #         os.path.join(map_merge_launch_dir, "slam_toolbox.py")
+            #     ),
+            #     condition=IfCondition(
+            #         PythonExpression(
+            #             [slam, " and ", slam_toolbox, " and not ", slam_gmapping]
+            #         )
+            #     ),
+            #     launch_arguments={
+            #         "use_sim_time": use_sim_time,
+            #     }.items(),
+            # ),
+            
+            # Collapsed slam_toolbox.py
+            start_sync_slam_toolbox_node,
+            
+            
+            
+            
+            
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
                     os.path.join(launch_dir, "localization_launch.py")
@@ -163,20 +228,6 @@ def generate_launch_description():
             ),
         ]
     )
-    # Not in GroupAction because namespace were prepended twice because 
-    # slam_gmapping.launch.py already accepts a namespace argument
-    # slam_gmapping_cmd = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         os.path.join(slam_gmapping_launch_dir, "slam_gmapping.launch.py")
-    #     ),
-    #     condition=IfCondition(
-    #         PythonExpression([slam, " and ", slam_gmapping, " and not ", slam_toolbox])
-    #     ),
-    #     launch_arguments={
-    #         "namespace": namespace,
-    #         "use_sim_time": use_sim_time,
-    #     }.items(),
-    # )
 
     # Create the launch description and populate
     ld = LaunchDescription()
@@ -189,11 +240,11 @@ def generate_launch_description():
     ld.add_action(declare_use_namespace_cmd)
     ld.add_action(declare_slam_cmd)
     ld.add_action(declare_slam_toolbox_cmd)
-    ld.add_action(declare_slam_gmapping_cmd)
     ld.add_action(declare_map_yaml_cmd)
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_params_file_cmd)
     ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_slam_params_file_cmd)
 
     # Add the actions to launch all of the navigation nodes
     ld.add_action(bringup_cmd_group)
